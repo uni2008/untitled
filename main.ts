b7fb8604c63aba8e6aa38e966b13d3c756aae957
uni2008 <93262463+uni2008@users.noreply.github.com>
2021-10-29 14:03:10 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+basic.forever(function () {
+    if (input.soundLevel() < 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (input.soundLevel() == 20) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (input.soundLevel() == 30) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() == 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() == 50) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() == 60) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+    	
+    }
+})
